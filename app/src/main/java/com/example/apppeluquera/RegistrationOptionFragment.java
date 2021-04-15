75d8ed9dcbb365d57b761836da00230e78f60dc2
SovietFalcon <ivancp3000@hotmail.es>
2021-04-15 20:25:52 +0200

    add register buttons

--- app/src/main/java/com/example/apppeluquera/RegistrationOptionFragment.java
+++ app/src/main/java/com/example/apppeluquera/RegistrationOptionFragment.java
@@ -2,11 +2,16 @@ package com.example.apppeluquera;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +20,32 @@ import android.view.ViewGroup;
  */
 public class RegistrationOptionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RegistrationOptionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistrationOptionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrationOptionFragment newInstance(String param1, String param2) {
-        RegistrationOptionFragment fragment = new RegistrationOptionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button register_admin_button;
+    Button register_user_button;
+    NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registration_option, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        navController = Navigation.findNavController(view);
+        register_admin_button = view.findViewById(R.id.register_admin_button);
+        register_user_button = view.findViewById(R.id.register_user_button);
+
+        register_admin_button.setOnClickListener(view1 -> {
+            navController.navigate(R.id.action_registrationOptionFragment_to_adminRegistrationFragment);
+        });
+
+        register_user_button.setOnClickListener(view2 -> {
+            navController.navigate(R.id.action_registrationOptionFragment_to_registrationFragment);
+        });
+
+
     }
 }
