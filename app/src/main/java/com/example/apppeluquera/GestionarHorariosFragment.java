c2c40a321fa3d211bd3e53e5985c54bff613e5e6
SovietFalcon <ivancp3000@hotmail.es>
2021-05-26 02:04:42 +0200

    -

--- app/src/main/java/com/example/apppeluquera/GestionarHorariosFragment.java
+++ app/src/main/java/com/example/apppeluquera/GestionarHorariosFragment.java
@@ -2,16 +2,31 @@ package com.example.apppeluquera;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class GestionarHorariosFragment extends Fragment {
+import com.example.apppeluquera.databinding.FragmentGestionarHorariosBinding;
+import com.example.apppeluquera.databinding.FragmentMenuNegocioBinding;
 
+public class GestionarHorariosFragment extends BaseFragment {
+
+    private FragmentGestionarHorariosBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gestionar_horarios, container, false);
+        return (binding = FragmentGestionarHorariosBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+
     }
 }
